b6827a1f35f6357d58761b8f79289237b5b2f8b6
nikhilm19 <nikmul19@gmail.com>
2020-09-06 17:57:35 +0530

    Updates

---
+++ precache-manifest.cb45f2c9d67ce722f7ed12c792a76acd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f607678298bdf95d9ee2e21cc636696",
+    "url": "/firecompass-fs-task/index.html"
+  },
+  {
+    "revision": "a1ae257ffc6c32a75956",
+    "url": "/firecompass-fs-task/static/css/main.32b1287f.chunk.css"
+  },
+  {
+    "revision": "62e77d37eff76fb00ab1",
+    "url": "/firecompass-fs-task/static/js/2.2548926a.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/firecompass-fs-task/static/js/2.2548926a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ae257ffc6c32a75956",
+    "url": "/firecompass-fs-task/static/js/main.cd0b0366.chunk.js"
+  },
+  {
+    "revision": "81ffb032bac65d16fb2c",
+    "url": "/firecompass-fs-task/static/js/runtime-main.f028dfaa.js"
+  },
+  {
+    "revision": "ec1ce54f7defd4d9a32da509edfcb9d9",
+    "url": "/firecompass-fs-task/static/media/1.ec1ce54f.jpg"
+  },
+  {
+    "revision": "765b9ad33ae435823917692e103de5f1",
+    "url": "/firecompass-fs-task/static/media/2.765b9ad3.jpg"
+  },
+  {
+    "revision": "68285dd7778514a361dcbc17808256fd",
+    "url": "/firecompass-fs-task/static/media/3.68285dd7.jpg"
+  },
+  {
+    "revision": "4f15f244995b85ca33ba2d298976f813",
+    "url": "/firecompass-fs-task/static/media/4.4f15f244.jpg"
+  },
+  {
+    "revision": "2cf55feae56e95fd92e18bf4d39b74fd",
+    "url": "/firecompass-fs-task/static/media/5.2cf55fea.jpg"
+  },
+  {
+    "revision": "b57f6120f3e67f3c0debdb20d10c4aa8",
+    "url": "/firecompass-fs-task/static/media/6.b57f6120.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firecompass-fs-task/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3ad551860b092c37ecd6c910dad57b05",
+    "url": "/firecompass-fs-task/static/media/wave.3ad55186.svg"
+  }
+]);
